1a272f62f580e2014b075ac1d8b842891ea9c7b2
Marcelo Puebla <marcenp23@gmail.com>
2025-09-07 01:44:14 -0300

    style(Timer): se actualizan los estilos del componente Timer, aumentando el tamaño de fuente a 7rem y ajustando el line-height.

--- src/components/Timer/Timer.jsx
+++ src/components/Timer/Timer.jsx
@@ -3,5 +3,9 @@ import { timer } from './Timer.module.css';
 
 export const Timer = ({ time }) => {
   const { minutes, seconds } = time;
-  return <div className={timer}>{`${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`}</div>;
+  return (
+    <div className={`t-bold t-600 ${timer}`}>{`${minutes.toString().padStart(2, '0')}:${seconds
+      .toString()
+      .padStart(2, '0')}`}</div>
+  );
 };
