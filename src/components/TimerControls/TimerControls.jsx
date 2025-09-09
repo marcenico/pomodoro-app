5220f482f7305dfdc7e8d7dc9caac9e436be8f17
Marcelo Puebla <marcenp23@gmail.com>
2025-09-09 15:01:10 -0300

    feat(assets, TimerControls): se eliminan archivos SVG no utilizados y se integran los íconos directamente en los componentes TimerControls y Header, mejorando la eficiencia y reduciendo la dependencia de archivos externos.

--- src/components/TimerControls/TimerControls.jsx
+++ src/components/TimerControls/TimerControls.jsx
@@ -10,10 +10,6 @@ import {
   btnStart,
   btnStartIcon
 } from './TimerControls.module.css';
-import pauseIcon from '/assets/pause.svg';
-import playIcon from '/assets/play.svg';
-import refreshIcon from '/assets/refresh.svg';
-import stopIcon from '/assets/stop.svg';
 
 export const TimerControls = ({ startTimer, resetTimer, pauseTimer, stopTimer, refreshTimer, isPaused, isRunning }) => {
   const [showButtons, setShowButtons] = useState(false);
@@ -53,22 +49,37 @@ export const TimerControls = ({ startTimer, resetTimer, pauseTimer, stopTimer, r
             viewBox="0 0 24 24"
             fill="currentColor"
             xmlns="http://www.w3.org/2000/svg">
-            <use href={refreshIcon} />
+            <path
+              d="M2 12C2 14.3869 2.94821 16.6761 4.63604 18.364C6.32387 20.0518 8.61305 21 11 21C13.39 21 15.68 20.06 17.4 18.4L15.9 16.9C15.2704 17.5667 14.5107 18.0971 13.6679 18.4583C12.825 18.8196 11.917 19.0039 11 19C4.76 19 1.64 11.46 6.05 7.05C10.46 2.64 18 5.77 18 12H15L19 16H19.1L23 12H20C20 9.61305 19.0518 7.32387 17.364 5.63604C15.6761 3.94821 13.3869 3 11 3C8.61305 3 6.32387 3.94821 4.63604 5.63604C2.94821 7.32387 2 9.61305 2 12Z"
+              fill="currentColor"
+            />
           </svg>
         </button>
       )}
       <button
         className={`d-flex ai-center jc-center t-0 ${btn} ${btnStart}`}
         onClick={isPaused ? startTimer : pauseTimer}>
-        <svg
-          className={btnStartIcon}
-          width="72"
-          height="72"
-          viewBox="0 0 72 72"
-          fill="currentColor"
-          xmlns="http://www.w3.org/2000/svg">
-          <use href={isPaused ? playIcon : pauseIcon} />
-        </svg>
+        {isPaused ? (
+          <svg
+            className={btnStartIcon}
+            width="72"
+            height="72"
+            viewBox="0 0 72 72"
+            fill="currentColor"
+            xmlns="http://www.w3.org/2000/svg">
+            <path d="M24 15.42V57.42L57 36.42L24 15.42Z" fill="currentColor" />
+          </svg>
+        ) : (
+          <svg
+            className={btnStartIcon}
+            width="72"
+            height="72"
+            viewBox="0 0 72 72"
+            fill="currentColor"
+            xmlns="http://www.w3.org/2000/svg">
+            <path d="M42 57H54V15H42M18 57H30V15H18V57Z" fill="currentColor" />
+          </svg>
+        )}
       </button>
       {showButtons && (
         <button
@@ -87,7 +98,7 @@ export const TimerControls = ({ startTimer, resetTimer, pauseTimer, stopTimer, r
             viewBox="0 0 24 24"
             fill="currentColor"
             xmlns="http://www.w3.org/2000/svg">
-            <use href={stopIcon} />
+            <path d="M18 18H6V6H18V18Z" fill="currentColor" />
           </svg>
         </button>
       )}
