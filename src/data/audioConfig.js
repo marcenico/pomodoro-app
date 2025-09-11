f515eb734e8d2a4e26757daa30b6d05174ec11f6
Marcelo Puebla <marcenp23@gmail.com>
2025-09-11 00:24:24 -0300

    refactor(data): actualizar configuración de audio para descanso largo

--- src/data/audioConfig.js
+++ src/data/audioConfig.js
@@ -13,9 +13,9 @@ export const audioConfig = {
     volume: 0.2 // Volume level (0.0 to 1.0)
   },
   longBreak: {
-    frequencies: [261.63, 329.63, 392.0], // C4, E4, G4 (C major chord lower)
-    duration: 0.6,
-    delay: 120,
-    volume: 0.2
+    frequencies: [523.25, 659.25, 783.99, 1046.5], // C5, E5, G5, C6 (C major chord higher + octave)
+    duration: 0.5,
+    delay: 100,
+    volume: 0.25
   }
 };
