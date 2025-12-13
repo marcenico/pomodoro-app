4a1debe6be3cb3649b9d35fdec617d5a40424cf1
Marcelo Puebla <marcenp23@gmail.com>
2025-12-13 20:21:34 -0300

    feat(components): add footer link button styles and update Footer component

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { footer, footerText } from './Footer.module.css';
+import { footer, footerText, footerLinkButton } from './Footer.module.css';
 
 export const Footer = () => {
   return (
@@ -7,11 +7,11 @@ export const Footer = () => {
       <div className="d-flex jc-center ai-center w-100 gap-32">
         {/* Made with ❤️ by */}
         <p className={footerText}>Made with ❤️ by&nbsp;
-          <a className={footerText} href="https://marcenico.com" target="_blank" rel="noopener noreferrer">marcenico</a>
+          <a className={footerLinkButton} href="https://marcenico.com" target="_blank" rel="noopener noreferrer">marcenico</a>
         </p>
         {/* Instagram */}
-        <a className={`${footerText} d-flex ai-center f-row gap-4`} href="https://www.instagram.com/_marcenico" target="_blank" rel="noopener noreferrer">
-          <span className="">Let's connect</span>
+        <a className={` ${footerLinkButton} d-flex ai-center f-row gap-4`} href="https://www.instagram.com/_marcenico" target="_blank" rel="noopener noreferrer">
+          <span className={footerText}>Let's connect</span>
           <svg width="18" height="18" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><use href="#instagram" /></svg>
         </a>
       </div>
