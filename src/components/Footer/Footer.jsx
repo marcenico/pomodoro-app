ef73ac1f9b1b8ab4b29fe217992775dad6e083ed
Marcelo Puebla <marcenp23@gmail.com>
2025-12-13 19:27:04 -0300

    feat(components): enhance Footer with Instagram link and update styles

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,14 +2,18 @@ import React from 'react';
 import { footer, footerText } from './Footer.module.css';
 
 export const Footer = () => {
-  const currentYear = new Date().getFullYear();
-
   return (
     <footer className={footer}>
-      <div className="d-flex jc-center ai-center w-100">
-        <p className={footerText}>
-          {currentYear} - The Pomodoro Technique
+      <div className="d-flex jc-center ai-center w-100 gap-32">
+        {/* Made with ❤️ by */}
+        <p className={footerText}>Made with ❤️ by&nbsp;
+          <a className={footerText} href="https://marcenico.com" target="_blank" rel="noopener noreferrer">marcenico</a>
         </p>
+        {/* Instagram */}
+        <a className={`${footerText} d-flex ai-center f-row gap-4`} href="https://www.instagram.com/_marcenico" target="_blank" rel="noopener noreferrer">
+          <span className="">Let's connect</span>
+          <svg width="18" height="18" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><use href="#instagram" /></svg>
+        </a>
       </div>
     </footer>
   );
