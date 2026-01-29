a13ed3a50c6361d66f39e5e131871c536ec5790e
Marcelo Puebla <marcenp23@gmail.com>
2026-01-29 18:58:34 -0300

    feat(components): update Footer with LinkedIn and X icons, enhance styles

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -9,11 +9,21 @@ export const Footer = () => {
         <p className={footerText}>Made with ❤️ by&nbsp;
           <a className={footerLinkButton} href="https://marcenico.com" target="_blank" rel="noopener noreferrer">marcenico</a>
         </p>
-        {/* Instagram */}
-        <a className={` ${footerLinkButton} d-flex ai-center f-row gap-4`} href="https://www.instagram.com/_marcenico" target="_blank" rel="noopener noreferrer">
+
+        {/* Let's connect */}
+        <div className="d-flex jc-center ai-center f-row gap-8">
           <span className={footerText}>Let's connect</span>
-          <svg width="18" height="18" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><use href="#instagram" /></svg>
-        </a>
+
+          {/* LinkedIn */}
+          <a className={` ${footerLinkButton} d-flex ai-center f-row gap-4`} href="https://www.linkedin.com/in/marcenico/" target="_blank" rel="noopener noreferrer" aria-label="LinkedIn">
+            <svg width="18" height="18" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><use href="#linkedin" /></svg>
+          </a>
+
+          {/* X */}
+          <a className={` ${footerLinkButton} d-flex ai-center f-row gap-4`} href="https://x.com/_marcenico" target="_blank" rel="noopener noreferrer">
+            <svg width="18" height="18" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><use href="#x" /></svg>
+          </a>
+        </div>
       </div>
     </footer>
   );
